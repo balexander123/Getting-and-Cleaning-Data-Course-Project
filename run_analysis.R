9bc02faf09454ff409be8cad8534f12047dbeaa6
balexander123 <barry.alexander@gmail.com>
2016-10-11 22:28:06 -0700

    [BA] tidy data file 1 candidate

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-setwd("/Users/ba25714/Getting-and-Cleaning-Data-Course-Project")
+setwd("~/coursera/GettingCleaning/CourseProject")
 
 DownloadData <- function() {
   if(!file.exists("data")){dir.create("data")}
@@ -7,12 +7,7 @@ DownloadData <- function() {
   unzip("data/UCI_DataSet.zip")
 }
 
-MakeTidy1 <- function(dataRootDir = "UCI HAR Dataset") {
-  
-  # Utility functions
-  MakePath <- function(file) {
-    paste(dataRootDir,"/",file,sep="")
-  }
+Setup <- function() {
   
   InstallRequire <- function(package) {
     install.packages(package)
@@ -24,7 +19,14 @@ MakeTidy1 <- function(dataRootDir = "UCI HAR Dataset") {
   
   # Install required packages
   InstallRequire("dplyr")
+}
 
+MakeTidy1 <- function(dataRootDir = "UCI HAR Dataset") {
+
+  MakePath <- function(file) {
+    paste(dataRootDir,"/",file,sep="")
+  }
+  
   # Load files
   kX_test_txt <- MakePath("test/X_test.txt")
   kX_train_txt <- MakePath("train/X_train.txt")
@@ -35,10 +37,48 @@ MakeTidy1 <- function(dataRootDir = "UCI HAR Dataset") {
   kTest_subject_test_txt <- MakePath("test/subject_test.txt")
   kTrain_subject_train_txt <- MakePath("train/subject_train.txt")
 
-  #merge training and test sets
+  # Merge training and test sets
   testSet <- read.table(kX_test_txt)
   trainingSet <- read.table(kX_train_txt)
   allObservations <- dplyr::bind_rows(testSet,trainingSet)
+  
+  allObservations
+  
+  # Descriptive names for columns
+  featureNames <- read.table(kFeatures_txt,stringsAsFactors=FALSE)[[2]]
+  colnames(allObservations) <- featureNames
+  # take only mean, std, or activityLabel columns
+  allObservations <- allObservations[,grep("mean|std|activityLabel",featureNames)]
+  
+  obsNames = names(allObservations)
+  obsNames <- gsub(pattern="^t",replacement="time",x=obsNames)
+  obsNames <- gsub(pattern="^f",replacement="freq",x=obsNames)
+  obsNames <- gsub(pattern="-?mean[(][)]-?",replacement="Mean",x=obsNames)
+  obsNames <- gsub(pattern="-?std[()][)]-?",replacement="Std",x=obsNames)
+  obsNames <- gsub(pattern="-?meanFreq[()][)]-?",replacement="MeanFreq",x=obsNames)
+  obsNames <- gsub(pattern="BodyBody",replacement="Body",x=obsNames)
+  names(allObservations) <- obsNames
+  
+  activityLabels <- read.table(kActivity_labels_txt,stringsAsFactors=FALSE)
+  colnames(activityLabels) <- c("activityID","activityLabel")
+  
+  testActivities <- read.table(kTest_y_test_txt,stringsAsFactors=FALSE)
+  trainingActivities <- read.table(kTrain_y_train_txt,stringsAsFactors=FALSE)
+  allActivities <- dplyr::bind_rows(testActivities,trainingActivities)
+  # Add activityID for joining
+  colnames(allActivities)[1] <- "activityID"
+  # Join by activityLabels
+  activities <- dplyr::inner_join(allActivities,activityLabels,by="activityID")
+  allObservations <- cbind(activity=activities[,"activityLabel"],allObservations)
+  
+  testSubjects <- read.table(kTest_subject_test_txt,stringsAsFactors=FALSE)
+  trainingSubjects <- read.table(kTrain_subject_train_txt,stringsAsFactors=FALSE)
+  allSubjects <- dplyr::bind_rows(testSubjects,trainingSubjects)
+  
+  colnames(allSubjects) <- "subject"
+  allObservations <- cbind(allSubjects,allObservations)
+  
+  allObservations <- dplyr::arrange(allObservations,subject,activity)
   
   allObservations
 }
